3a4f1518504c78a903bd091c59720764ecc3ced7
Sam Joan Roque-Worcel <sam@roque-worcel.com>
2024-12-02 21:30:52 +1300

    rubocop :shock:

--- 2/main_part1.rb
+++ 2/main_part1.rb
@@ -1,43 +1,33 @@
-left = []
-right = []
+# frozen_string_literal: true
 
 safe_levels = 0
-File.open(ARGV[0], "r") do |f|
+File.open(ARGV[0], 'r') do |f|
   f.each_line do |line|
     levels = line.split(' ')
 
-    if levels[1].to_i > levels[0].to_i
-      should_be_increasing = true
-    else
-      should_be_increasing = false
-    end
+    should_be_increasing = levels[1].to_i > levels[0].to_i
 
     safe = true
     levels.each_index do |i|
       curr_level = levels[i].to_i
       next_level = levels[i + 1].to_i
-      if levels[i + 1]
-        diff = next_level - curr_level
-        if diff.abs == 0 or diff.abs > 3
-          safe = false
-          break
-        else
-          if should_be_increasing && diff < 0
-            safe = false
-            break
-          elsif !should_be_increasing && diff > 0
-            safe = false
-            break
-          end
-        end
+      next unless levels[i + 1]
+
+      diff = next_level - curr_level
+      if diff.abs.zero? || (diff.abs > 3)
+        safe = false
+        break
+      elsif should_be_increasing && diff.negative?
+        safe = false
+        break
+      elsif !should_be_increasing && diff.positive?
+        safe = false
+        break
       end
     end
-    
-    if safe
-      safe_levels += 1
-    end
+
+    safe_levels += 1 if safe
   end
 end
 
 puts safe_levels
-

813d3e78bc1409b37e40242b5eef08bd3da76e6f
Sam Joan Roque-Worcel <sam@roque-worcel.com>
2024-12-05 21:00:27 +1300

    Code up part 5 day 1 and 2.

---
+++ 5/main.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+def is_update_valid?(update, rules)
+  rules.each do |rule|
+    before, after = rule
+    before_pos = update.index(before)
+    after_pos = update.index(after)
+    
+    both_elements_present = before_pos && after_pos
+
+    if both_elements_present && !(before_pos < after_pos)
+      return false
+    end
+  end
+
+  true
+end
+
+def reorder_invalid_update(update, rules)
+  rules.each do |rule|
+    before, after = rule
+    before_pos = update.index(before)
+    after_pos = update.index(after)
+    
+    both_elements_present = before_pos && after_pos
+
+    if both_elements_present && !(before_pos < after_pos)
+      new_update = update.dup
+      new_update[after_pos] = before
+      new_update[before_pos] = after
+      
+      return reorder_invalid_update(new_update, rules)
+    end
+  end
+
+  update
+end
+
+rules = []
+updates = []
+File.open(ARGV[0], 'r') do |f|
+  parsing_page_numbers = false
+  f.each_line do |line|
+    if line == "\n"
+      parsing_page_numbers = true
+      next
+    end
+
+    if not parsing_page_numbers
+      before, after = line.strip.split('|')
+      rules.append([before, after])
+    else
+      update = line.strip.split(',')
+      updates.append(update)
+    end
+  end
+end
+
+total = 0
+updates.each do |update|
+  if !is_update_valid?(update, rules)
+    middle_index = (update.length - 1) / 2
+    reordered = reorder_invalid_update(update, rules)
+    total += reordered[middle_index].to_i
+  end
+end
+
+puts total

97d040353216c4a75a7b532039b0c51f62911225
Sam Joan Roque-Worcel <sam@roque-worcel.com>
2024-12-05 19:53:17 +1300

    Rubocop.

--- 4/main.rb
+++ 4/main.rb
@@ -4,56 +4,56 @@ class OutOfBounds < StandardError
 end
 
 class XmasFinder
-
   def initialize(puzzle)
     @puzzle = puzzle
   end
 
   def get_elem(y, x)
-    raise OutOfBounds if (x.negative? || y.negative?) || 
-      (y > @puzzle.length - 1) || (x > @puzzle[0].length - 1)
+    raise OutOfBounds if (x.negative? || y.negative?) ||
+                         (y > @puzzle.length - 1) || (x > @puzzle[0].length - 1)
 
     @puzzle[y][x]
   end
 
-  def horizontal_right(puzzle, x, y)
-    get_elem(y, x) == "X" && get_elem(y, x+1) == "M" && get_elem(y, x+2) == "A" && get_elem(y, x+3) == "S"
+  def horizontal_right(x, y)
+    get_elem(y, x) == 'X' && get_elem(y, x + 1) == 'M' && get_elem(y, x + 2) == 'A' && get_elem(y, x + 3) == 'S'
   end
 
-  def horizontal_left(puzzle, x, y)
-    get_elem(y, x) == "X" && get_elem(y, x-1) == "M" && get_elem(y, x-2) == "A" && get_elem(y, x-3) == "S"
+  def horizontal_left(x, y)
+    get_elem(y, x) == 'X' && get_elem(y, x - 1) == 'M' && get_elem(y, x - 2) == 'A' && get_elem(y, x - 3) == 'S'
   end
 
-  def vertical_up(puzzle, x, y)
-    get_elem(y, x) == "X" && get_elem(y-1, x) == "M" && get_elem(y-2, x) == "A" && get_elem(y-3, x) == "S"
+  def vertical_up(x, y)
+    get_elem(y, x) == 'X' && get_elem(y - 1, x) == 'M' && get_elem(y - 2, x) == 'A' && get_elem(y - 3, x) == 'S'
   end
 
-  def vertical_down(puzzle, x, y)
-    get_elem(y, x) == "X" && get_elem(y+1, x) == "M" && get_elem(y+2, x) == "A" && get_elem(y+3, x) == "S"
+  def vertical_down(x, y)
+    get_elem(y, x) == 'X' && get_elem(y + 1, x) == 'M' && get_elem(y + 2, x) == 'A' && get_elem(y + 3, x) == 'S'
   end
 
-  def diagonal_right_up(puzzle, x, y)
-    get_elem(y, x) == "X" && get_elem(y-1, x+1) == "M" && get_elem(y-2, x+2) == "A" && get_elem(y-3, x+3) == "S"
+  def diagonal_right_up(x, y)
+    get_elem(y, x) == 'X' && get_elem(y - 1, x + 1) == 'M' && get_elem(y - 2, x + 2) == 'A' && get_elem(y - 3, x + 3) == 'S'
   end
 
-  def diagonal_right_down(puzzle, x, y)
-    get_elem(y, x) == "X" && get_elem(y+1, x+1) == "M" && get_elem(y+2, x+2) == "A" && get_elem(y+3, x+3) == "S"
+  def diagonal_right_down(x, y)
+    get_elem(y, x) == 'X' && get_elem(y + 1, x + 1) == 'M' && get_elem(y + 2, x + 2) == 'A' && get_elem(y + 3, x + 3) == 'S'
   end
 
-  def diagonal_left_up(puzzle, x, y)
-    get_elem(y, x) == "X" && get_elem(y-1, x-1) == "M" && get_elem(y-2, x-2) == "A" && get_elem(y-3, x-3) == "S"
+  def diagonal_left_up(x, y)
+    get_elem(y, x) == 'X' && get_elem(y - 1, x - 1) == 'M' && get_elem(y - 2, x - 2) == 'A' && get_elem(y - 3, x - 3) == 'S'
   end
 
-  def diagonal_left_down(puzzle, x, y)
-    get_elem(y, x) == "X" && get_elem(y+1, x-1) == "M" && get_elem(y+2, x-2) == "A" && get_elem(y+3, x-3) == "S"
+  def diagonal_left_down(x, y)
+    get_elem(y, x) == 'X' && get_elem(y + 1, x - 1) == 'M' && get_elem(y + 2, x - 2) == 'A' && get_elem(y + 3, x - 3) == 'S'
   end
 end
 
-finders = [:horizontal_right, :horizontal_left, :vertical_up, :vertical_down, :diagonal_right_up, :diagonal_right_down, :diagonal_left_up, :diagonal_left_down]
+finders = %i[horizontal_right horizontal_left vertical_up vertical_down diagonal_right_up diagonal_right_down
+             diagonal_left_up diagonal_left_down]
 
 puzzle = nil
 File.open(ARGV[0], 'r') do |f|
-  puzzle = f.readlines chomp:true
+  puzzle = f.readlines chomp: true
 end
 
 total = 0
@@ -62,11 +62,8 @@ puzzle.each.with_index do |line, y|
   line.each_char.with_index do |char, x|
     if char == 'X'
       finders.each do |method_name|
-        begin
-	  if xmas_finder.send(method_name, puzzle, x, y)
-	    total += 1 
-	  end
-        rescue OutOfBounds; end
+        total += 1 if xmas_finder.send(method_name, x, y)
+      rescue OutOfBounds
       end
     end
   end

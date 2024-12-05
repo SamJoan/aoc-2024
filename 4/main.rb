8bc24029dbad7356ee36a5e4ce034a8f46b15725
Sam Joan Roque-Worcel <sam@roque-worcel.com>
2024-12-05 20:17:38 +1300

    Solve part 4.

--- 4/main.rb
+++ 4/main.rb
@@ -15,58 +15,57 @@ class XmasFinder
     @puzzle[y][x]
   end
 
-  def horizontal_right(x, y)
-    get_elem(y, x) == 'X' && get_elem(y, x + 1) == 'M' && get_elem(y, x + 2) == 'A' && get_elem(y, x + 3) == 'S'
-  end
-
-  def horizontal_left(x, y)
-    get_elem(y, x) == 'X' && get_elem(y, x - 1) == 'M' && get_elem(y, x - 2) == 'A' && get_elem(y, x - 3) == 'S'
-  end
-
-  def vertical_up(x, y)
-    get_elem(y, x) == 'X' && get_elem(y - 1, x) == 'M' && get_elem(y - 2, x) == 'A' && get_elem(y - 3, x) == 'S'
-  end
-
-  def vertical_down(x, y)
-    get_elem(y, x) == 'X' && get_elem(y + 1, x) == 'M' && get_elem(y + 2, x) == 'A' && get_elem(y + 3, x) == 'S'
-  end
-
   def diagonal_right_up(x, y)
-    get_elem(y, x) == 'X' && get_elem(y - 1, x + 1) == 'M' && get_elem(y - 2, x + 2) == 'A' && get_elem(y - 3, x + 3) == 'S'
+    if get_elem(y, x) == 'M' && get_elem(y - 1, x + 1) == 'A' && get_elem(y - 2, x + 2) == 'S'
+      return y - 1, x + 1
+    end
   end
 
   def diagonal_right_down(x, y)
-    get_elem(y, x) == 'X' && get_elem(y + 1, x + 1) == 'M' && get_elem(y + 2, x + 2) == 'A' && get_elem(y + 3, x + 3) == 'S'
+    if get_elem(y, x) == 'M' && get_elem(y + 1, x + 1) == 'A' && get_elem(y + 2, x + 2) == 'S'
+      return y + 1, x + 1
+    end
   end
 
   def diagonal_left_up(x, y)
-    get_elem(y, x) == 'X' && get_elem(y - 1, x - 1) == 'M' && get_elem(y - 2, x - 2) == 'A' && get_elem(y - 3, x - 3) == 'S'
+    if get_elem(y, x) == 'M' && get_elem(y - 1, x - 1) == 'A' && get_elem(y - 2, x - 2) == 'S'
+      return y - 1, x - 1
+    end
   end
 
   def diagonal_left_down(x, y)
-    get_elem(y, x) == 'X' && get_elem(y + 1, x - 1) == 'M' && get_elem(y + 2, x - 2) == 'A' && get_elem(y + 3, x - 3) == 'S'
+    if get_elem(y, x) == 'M' && get_elem(y + 1, x - 1) == 'A' && get_elem(y + 2, x - 2) == 'S'
+      return y + 1, x - 1
+    end
   end
 end
 
-finders = %i[horizontal_right horizontal_left vertical_up vertical_down diagonal_right_up diagonal_right_down
-             diagonal_left_up diagonal_left_down]
+finders = %i[diagonal_right_up diagonal_right_down diagonal_left_up diagonal_left_down]
 
 puzzle = nil
 File.open(ARGV[0], 'r') do |f|
   puzzle = f.readlines chomp: true
 end
 
-total = 0
 xmas_finder = XmasFinder.new(puzzle)
+letter_a_locations = []
 puzzle.each.with_index do |line, y|
   line.each_char.with_index do |char, x|
-    if char == 'X'
+    if char == 'M'
       finders.each do |method_name|
-        total += 1 if xmas_finder.send(method_name, x, y)
+        location_of_letter_a = xmas_finder.send(method_name, x, y)
+        if location_of_letter_a
+          letter_a_locations.append(location_of_letter_a)
+        end
       rescue OutOfBounds
       end
     end
   end
+end
+
+total = 0
+letter_a_locations.tally().each_pair do |coord, nb_of_mas|
+  total += 1 if nb_of_mas >= 2
 end
 
 puts total

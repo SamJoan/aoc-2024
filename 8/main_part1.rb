78e64302f2818d6fa41c43fa4785f26ef48f4ccd
Sam Joan Roque-Worcel <sam@roque-worcel.com>
2024-12-09 20:38:41 +1300

    Solve day 8, part 1 & 2.

---
+++ 8/main_part1.rb
@@ -0,0 +1,60 @@
+def get_char_locations(map)
+  chars = {}
+  map.each.with_index do |line, y|
+    line.each_char.with_index do |char, x|
+      if char != '.'
+        if !chars[char]
+          chars[char] = []
+        end
+        chars[char].append([x, y])
+      end
+    end
+  end
+
+  chars
+end
+
+def debug(map)
+  map.each do |line|
+    p line
+  end
+end
+
+def coords_valid?(map, coords)
+  x, y = coords
+  invalid = (x > map[0].length - 1 || x.negative?) ||
+    (y > map.length - 1 || y.negative?)
+  
+  !invalid
+end
+
+def populate_antinodes(map)
+  chars = get_char_locations(map)
+
+  chars.each do |char, value|
+    pairs = value.combination(2)
+    pairs.each do |pair|
+      # https://stackoverflow.com/questions/2682411/ruby-sum-corresponding-members-of-two-or-more-arrays
+
+      a, b = pair
+
+      distance = pair.transpose.map {|x| x.reduce(:-)}
+      antinode_a = [a[0] + distance[0] , a[1] + distance[1]]
+      antinode_b = [b[0] - distance[0] , b[1] - distance[1]]
+
+      map[antinode_a[1]][antinode_a[0]] = '#' if coords_valid?(map, antinode_a)
+      map[antinode_b[1]][antinode_b[0]] = '#' if coords_valid?(map, antinode_b)
+    end
+  end
+end
+  
+
+map = IO.readlines(ARGV[0]).map(&:strip)
+populate_antinodes(map)
+
+total = 0
+map.each do |line|
+  total += line.count('#')
+end
+
+puts total

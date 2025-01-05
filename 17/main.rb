3677994de112215e975430695e532c9dadbbbc39
Sam Joan Roque-Worcel <sam@roque-worcel.com>
2025-01-05 21:28:53 +1300

    Make brute force smarter.

--- 17/main.rb
+++ 17/main.rb
@@ -1,4 +1,74 @@
 # frozen_string_literal: true
+class Element
+
+  attr_accessor :cost
+
+  def initialize(cost, base_nb)
+    @cost = cost
+    @base_nb = base_nb
+  end
+
+  def get_values
+    return @cost, @base_nb
+  end
+end
+
+class MinHeap
+
+  def initialize
+    @data = []
+    @dirty = true
+  end
+
+  def <<(element)
+    @dirty = true
+    @data << element
+  end
+
+  def swap!(a, b)
+    temp = @data[b]
+    @data[b] = @data[a]
+    @data[a] = temp
+  end
+
+  def heapify!(i)
+    if @data.length > 1
+      smallest = i
+
+      left = 2 * i + 1
+      right = 2 * i + 2
+
+      smallest = left if left < @data.length && @data[left].cost < @data[smallest].cost
+      smallest = right if right < @data.length && @data[right].cost < @data[smallest].cost
+
+      if smallest != i
+        swap!(i, smallest)
+        heapify!(smallest)
+      end
+    end
+  end
+
+  def get_min
+    if @data.length > 0
+      if @dirty
+        (@data.length / 2 - 1).downto(0) do |i|
+          heapify!(i)
+          @dirty = false
+        end
+      end
+
+      elem = @data[0]
+      swap!(0, @data.length - 1)
+      @data.pop
+
+      heapify!(0)
+    end
+
+    elem
+  end
+end
+
+
 
 class Program
 
@@ -135,7 +205,15 @@ def parse_input(filename)
   Program.new(a, b, c, instructions)
 end
 
-def get_diff_indexes(input_a, input_b)
+def get_diff_indexes(base_nb, index)
+  base_value = base_nb[index]
+  sample_a = modify_char_at_index(base_nb, index, (base_value.to_i + 1) % 8)
+  sample_b = modify_char_at_index(base_nb, index, (base_value.to_i + 2) % 8)
+
+  list_diff(sample_a, sample_b)
+end
+
+def list_diff(input_a, input_b)
   different_indexes = []
   input_a.each.with_index do |a, i|
     different_indexes.append(i) if a != input_b[i]
@@ -167,36 +245,83 @@ def modify_char_at_index(base_nb, index, nb)
   new_output
 end
 
+def modify_base_nb(base_nb, index, nb)
+  new_nb = base_nb.dup
+  new_nb[index] = nb.to_s
+
+  new_nb
+end
+
+def calculate_distance(target_result, base_nb)
+  output = get_output(base_nb)
+
+  list_diff(output, target_result).length
+end
+
 program_class = parse_input(ARGV[0])
 original_program = program_class.instructions
 $frozen_program = Marshal.dump(program_class)
 
 #i = 04536555555555554
-base_nb = "6551100000021704" # this was me calculating manually by tweaking
+#base_nb = "6551100000021704" # this was me calculating manually by tweaking
   # with the numbers.
-base_nb = "6551100000021704"
+#base_nb = "6551100000021704"
+initial_base_nb = ARGV[1]
 
-puts "New base number output: #{get_output(base_nb).join(',')}"
+puts "New base number output: #{get_output(initial_base_nb).join(',')}"
 puts "                        #{original_program.join(',')} (#{original_program.length})"
 puts 
 
-base_nb.each_char.with_index do |current_val, index|
-  sample_a = modify_char_at_index(base_nb, index, 2)
-  sample_b = modify_char_at_index(base_nb, index, 3)
-  different_indexes = get_diff_indexes(sample_a, sample_b)
-  last_result = nil
-  potentially_valid = []
-  (0..7).each do |nb|
-    output = modify_char_at_index(base_nb, index, nb)
-    any_chars_match = false
-    different_indexes.each do |diff_index|
-      if original_program[diff_index] == output[diff_index]
-        any_chars_match = true
+raise "Bad base_nb len" if get_output(initial_base_nb).length != original_program.length
+TARGET_OUTPUT = original_program.dup.freeze
+
+mh = MinHeap.new
+mh << Element.new(calculate_distance(TARGET_OUTPUT, initial_base_nb), initial_base_nb)
+
+already_seen = {}
+
+min_base_value = nil
+loop do
+  elem = mh.get_min
+  distance, base_nb = elem.get_values
+
+  if distance == 0
+    puts "Found target value #{base_nb} (oct -> int #{base_nb.oct}). Output of calculation: #{get_output(base_nb)}, target #{TARGET_OUTPUT}"
+    if !min_base_value
+      min_base_value = base_nb
+    else
+      if base_nb < min_base_value
+        min_base_value = base_nb
+        puts "[+] Newest min #{base_nb} (oct -> int #{base_nb.oct}). Output of calculation: #{get_output(base_nb)}, target #{TARGET_OUTPUT}"
+      end
+    end
+  end
+
+  already_seen[base_nb] = true
+  #puts "[+] #{distance} #{base_nb}"
+
+  base_output = get_output(base_nb)
+  base_nb.each_char.with_index do |current_val, index|
+    indexes_that_change = get_diff_indexes(base_nb, index)
+    all_equal = true
+    indexes_that_change.each do |index|
+      if base_output[index] != TARGET_OUTPUT[index]
+        all_equal = false
+        break
       end
     end
 
-    potentially_valid.append(nb) if any_chars_match
-  end
+    next if all_equal
 
-  puts "Potentially valid for index #{index}: #{potentially_valid.join(',')} (Modifies #{different_indexes.join(',')})"
+    range = index == 0 ? (1..7) : (0..7) 
+
+    range.each do |nb|
+      modified_base_nb = modify_base_nb(base_nb, index, nb)
+      next if modified_base_nb == base_nb
+      next if already_seen[modified_base_nb]
+
+      mh << Element.new(calculate_distance(TARGET_OUTPUT, modified_base_nb), modified_base_nb)
+    end
+
+  end
 end
